ad8a694ed5fec64d81ba52a669b03b53eb6a5251
Ruben Gees <geesruben@yahoo.de>
2017-01-10 02:08:06 +0100

    Cleanups

--- library/src/main/java/com/vanniktech/emoji/emoji/Emoji.java
+++ library/src/main/java/com/vanniktech/emoji/emoji/Emoji.java
@@ -7,8 +7,7 @@ import java.io.Serializable;
 public final class Emoji implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @NonNull
-    private final String emoji;
+    @NonNull private final String emoji;
 
     public Emoji(@NonNull final String emoji) {
         this.emoji = emoji;
